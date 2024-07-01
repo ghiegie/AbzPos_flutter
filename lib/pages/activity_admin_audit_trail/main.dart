d0c2e64eac5e953c48a87b6228470068b7b9668e
ghiegie <giancarlogarcia.gallano@gmail.com>
2024-07-01 11:10:13 +0800

    07-01-24

--- lib/pages/activity_admin_audit_trail/main.dart
+++ lib/pages/activity_admin_audit_trail/main.dart
@@ -1,3 +1,4 @@
+import 'package:data_table_2/data_table_2.dart';
 import 'package:flutter/material.dart';
 
 class AcitvityAdminAuditTrail extends StatefulWidget {
@@ -8,6 +9,44 @@ class AcitvityAdminAuditTrail extends StatefulWidget {
 }
 
 class _AcitvityAdminAuditTrailState extends State<AcitvityAdminAuditTrail> {
+  // DateTime? _date;
+  final _fromDate = TextEditingController();
+  final _toDate = TextEditingController();
+
+  void Function() showDate(TextEditingController ctrl, void Function()? addtional) {
+    return () {
+      showDatePicker(
+        context: context, 
+        firstDate: DateTime(0), 
+        lastDate: DateTime.now()
+      ).then((val) {
+        setState(() {
+          if (val == null) {
+            ctrl.text = ctrl.text;
+            return;
+          } 
+          ctrl.text = val.toString();
+        });
+      });
+    };
+  }
+
+  // void showDate() {
+  //   showDatePicker(
+  //     context: context, 
+  //     firstDate: DateTime(0), 
+  //     lastDate: DateTime.now()
+  //   ).then((val) {
+  //     setState(() {
+  //       if (val == null) {
+  //         _fromDate.text = _fromDate.text;
+  //         return;
+  //       } 
+  //       _fromDate.text = val.toString();
+  //     });
+  //   });
+  // }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,41 +71,183 @@ class _AcitvityAdminAuditTrailState extends State<AcitvityAdminAuditTrail> {
               children: [
                 const Text("FROM:"),
                 const SizedBox(width: 25),
-                const Expanded(
-                  child: TextField(
-                    decoration: null,
-                    // decoration: InputDecoration(
-                    //   border: OutlineInputBorder(
-                    //     borderRadius: BorderRadius.all(Radius.circular(5.0))
-                    //   )
-                    // ),
-                  )
+                Expanded(
+                  child: Container(
+                    padding: const EdgeInsets.only(
+                      left: 5
+                    ),
+                    // height: 50,
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.black
+                      )
+                    ),
+                    child: IntrinsicHeight(
+                      child: Row(
+                        children: [
+                          Expanded(
+                            child: TextField(
+                              decoration: null,
+                              readOnly: true,
+                              onTap: () {
+                                showDatePicker(
+                                  context: context, 
+                                  firstDate: DateTime(0), 
+                                  lastDate: DateTime.now()
+                                ).then((val) {
+                                  setState(() {
+                                    if (val == null) {
+                                      _fromDate.text = _fromDate.text;
+                                      return;
+                                    } 
+                                    _fromDate.text = val.toString();
+                                  });
+                                });
+                              },
+                              controller: _fromDate,
+                            ),
+                          ),
+                          const VerticalDivider(
+                            thickness: 1,
+                            width: 3,
+                            color: Colors.black,
+                          ),
+                          IconButton(
+                            onPressed: () {
+                              showDatePicker(
+                                context: context, 
+                                firstDate: DateTime(0), 
+                                lastDate: DateTime.now()
+                              ).then((val) {
+                                setState(() {
+                                  if (val == null) {
+                                    _fromDate.text = _fromDate.text;
+                                    return;
+                                  } 
+                                  _fromDate.text = val.toString();
+                                });
+                              });
+                            }, 
+                            icon: const Icon(Icons.arrow_drop_down)
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
                 ),
 
                 const SizedBox(width: 50),
 
                 const Text("TO:"),
                 const SizedBox(width: 25),
-                const Expanded(
-                  child: TextField(
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(5.0))
+                Expanded(
+                  child: Container(
+                    padding: const EdgeInsets.only(
+                      left: 5
+                    ),
+                    // height: 50,
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.black
                       )
                     ),
-                  )
+                    child: IntrinsicHeight(
+                      child: Row(
+                        children: [
+                          Expanded(
+                            child: TextField(
+                              decoration: null,
+                              readOnly: true,
+                              onTap: () {
+                                showDatePicker(
+                                  context: context, 
+                                  firstDate: () {
+                                    if (_fromDate.text == "") {
+                                      return DateTime(0);
+                                    }
+
+                                    var date = DateTime.parse(_fromDate.text);
+
+                                    return date;
+                                  }(), 
+                                  lastDate: DateTime.now()
+                                ).then((val) {
+                                  setState(() {
+                                    if (val == null) {
+                                      _toDate.text = _toDate.text;
+                                      return;
+                                    } 
+                                    _toDate.text = val.toString();
+                                  });
+                                });
+                              },
+                              controller: _toDate,
+                            ),
+                          ),
+                          const VerticalDivider(
+                            thickness: 1,
+                            width: 3,
+                            color: Colors.black,
+                          ),
+                          IconButton(
+                            onPressed: () {
+                                showDatePicker(
+                                  context: context, 
+                                  firstDate: () {
+                                    if (_fromDate.text == "") {
+                                      return DateTime(0);
+                                    }
+
+                                    var date = DateTime.parse(_fromDate.text);
+
+                                    return date;
+                                  }(), 
+                                  lastDate: DateTime.now()
+                                ).then((val) {
+                                  setState(() {
+                                    if (val == null) {
+                                      _toDate.text = _toDate.text;
+                                      return;
+                                    } 
+                                    _toDate.text = val.toString();
+                                  });
+                                });
+                              }, 
+                            icon: const Icon(Icons.arrow_drop_down)
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
                 ),
 
-                const SizedBox(width: 50),
+                const SizedBox(width: 5),
 
-                ElevatedButton(onPressed: () {}, child: const Text("asdasda"))
+                ElevatedButton(onPressed: () {}, child: const Text("asdasda")),
               ]
             ),
         
-            const Text("asdasd"),
-            const Text("asdasd"),
-            const Text("asdasd"),
-            const Text("asdasd"),
+            const SizedBox(height: 5),
+            
+            Expanded(
+              child: DataTable2(
+                border: TableBorder.all(
+                  color: Colors.black
+                ),
+                columns: [
+                  DataColumn2(label: const Text('asdasda')),
+                  DataColumn2(label: const Text('asdasda')),
+                  DataColumn2(label: const Text('asdasda')),
+                  DataColumn2(label: const Text('asdasda')),
+                  DataColumn2(label: const Text('asdasda')),
+                  DataColumn2(label: const Text('asdasda')),
+                  DataColumn2(label: const Text('asdasda')),
+                  DataColumn2(label: const Text('asdasda')),
+                  DataColumn2(label: const Text('asdasda')),
+                ], 
+                rows: []
+              )
+            )
           ]
         ),
       )
